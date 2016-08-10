662615dc4eb2490cc9636966d1c1d2807447615e
Sebastian Jug <sejug@redhat.com>
2016-08-10 08:23:05 -0400

    docker-entrypoint script improved consistency

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,15 @@ set -e
 
 case "${RUN}" in
   stress)
-    if [[ "${STRESS_CPU}" ]]; then STRESS_CPU="--cpu ${STRESS_CPU}"; fi
-    if [[ "${STRESS_TIME}" ]]; then STRESS_TIME="--timeout ${STRESS_TIME}"; fi
-    exec stress ${STRESS_CPU} ${STRESS_TIME}
+    [[ "${STRESS_CPU}" ]] && STRESS_CPU="--cpu ${STRESS_CPU}"
+    [[ "${STRESS_TIME}" ]] && STRESS_TIME="--timeout ${STRESS_TIME}"
+    exec stress "${STRESS_CPU}" "${STRESS_TIME}"
     ;;
   jmeter)
     [[ "${ROUTER_IP}" ]] && echo "${ROUTER_IP} ${TARGET_IP}" >> /etc/hosts
-    exec jmeter -n -t test.jmx -Jipaddr=${TARGET_IP} -Jport=${TARGET_PORT}
+    [[ "${TARGET_IP}" ]] && TARGET_IP="-Jipaddr=${TARGET_IP}"
+    [[ "${TARGET_PORT}" ]] && TARGET_PORT="-Jport=${TARGET_PORT}"
+    exec jmeter -n -t test.jmx "${TARGET_IP}" "${TARGET_PORT}"
     ;;
   *)
     echo "Need to specify what to run."
