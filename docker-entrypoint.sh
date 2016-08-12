d3d667777140fc6bb4164fa0cb24cb5b3b6cf538
Sebastian Jug <sejug@redhat.com>
2016-08-12 14:33:15 -0400

    Initial support of concurrent requests

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,10 +10,12 @@ case "${RUN}" in
     exec stress "${STRESS_CPU}" "${STRESS_TIME}"
     ;;
   jmeter)
+    IFS=$'\n' 
+    NUM="$(echo $TARGET_IP | sed 's/\//\n/g' | wc -l)"
+    TARGET=($(echo $TARGET_IP | sed 's/\//\n/g'))
     [[ "${ROUTER_IP}" ]] && echo "${ROUTER_IP} ${TARGET_IP}" >> /etc/hosts
-    [[ "${TARGET_IP}" ]] && TARGET_IP="-Jipaddr=${TARGET_IP}"
-    [[ "${TARGET_PORT}" ]] && TARGET_PORT="-Jport=${TARGET_PORT}"
-    exec jmeter -n -t test.jmx "${TARGET_IP}" "${TARGET_PORT}"
+    exec jmeter -n -t test.jmx -Jnum=${NUM} -Jipaddr1=${TARGET[0]} -Jipaddr2=${TARGET[1]} \
+      -Jipaddr3=${TARGET[2]} -Jipaddr4=${TARGET[3]} -Jipaddr5=${TARGET[4]} -Jport=${TARGET_PORT}
     ;;
   *)
     echo "Need to specify what to run."
